3456f703108f6827925dfea62ba238146c33e2cf
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-21 16:04:45 +0000

    lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -16,7 +16,8 @@ class LRUCache(BaseCaching):
         Initializer
         """
         super().__init__()
-
+        self.cache = OrderedDict()
+        self.cache = self.cache_data
     def put(self, key, item):
         """
         put for key items
@@ -24,20 +25,17 @@ class LRUCache(BaseCaching):
             key(str): key of dictionary
             item(Any): value of dictionary
         """
-        cache = self.cache_data
-
         if key is not None and item is not None:
-            if len(cache) >= BaseCaching.MAX_ITEMS:
-                lfu = next(iter(cache))
-                del cache[lfu]
+            if len(self.cache) >= BaseCaching.MAX_ITEMS:
+                lfu = next(iter(self.cache))
+                del self.cache[lfu]
                 print(f"DISCARD: {lfu}")
-            cache[key] = item
+            self.cache[key] = item
 
     def get(self, key):
         """
         get key
         """
-        cache = self.cache_data
-        if key is None or key not in cache:
+        if key is None or key not in self.cache:
             return None
-        return cache[key]
+        return self.cache[key]
