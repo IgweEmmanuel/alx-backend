9afe2276482c142414a0257d6c0c49c3fe15e2b9
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-28 00:47:38 +0000

    flask app

---
+++ 0x02-i18n/template/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+"""flask language translation"""
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """render 0-index"""
+    return render_template("0-index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host="localhost")
