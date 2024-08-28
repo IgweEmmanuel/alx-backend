c8202b0c7dbf26c72ae9f355fbbc6df08fbc036a
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-28 03:50:53 +0000

    3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -24,7 +24,7 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     """return 2-index"""
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
