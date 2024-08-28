24efd1781f13388be57bb08a99fce2e1835cd822
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-28 04:00:34 +0000

    3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
-"""How to use localeselector"""
-from flask import Flask, request, render_template
+"""Basic Flask app module"""
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
 class Config:
-    """language setup"""
+    """Flask Babel configuration"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale() -> str:
-    """return loclae selector for language"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """Retrieves the locale for a web page"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])  # noqa
+
 
 @app.route('/')
-def index() -> str:
-    """return 2-index"""
-    return render_template("3-index.html")
+def get_index() -> str:
+    """index page"""
+    return render_template('2-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
