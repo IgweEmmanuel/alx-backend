ff60df2dd96ad8a168533b609df080d74e574ee2
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-28 02:01:49 +0000

    2-app update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,21 +4,22 @@ from flask import Flask, request, render_template
 from flask_babel import Babel
 
 
-def get_locale():
-    """get local language"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-app = Flask(__name__)
-
 class Config:
     """language setup"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+app = Flask(__name__)
+
 app.config.from_object(Config)
 
 babel = Babel(app, local_selector=get_locale)
+
+@babel.localeselector
+def get_locale():
+    """return loclae selector for language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 @app.route('/')
 def index():
