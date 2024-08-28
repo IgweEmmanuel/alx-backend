f77698132a45b570c248cbbce61bab29d0b31eae
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-28 04:02:15 +0000

    3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Basic Flask app module"""
+"""Basic Flask app with internationalization support"""
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
@@ -20,13 +20,13 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale() -> str:
     """Retrieves the locale for a web page"""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])  # noqa
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
 def get_index() -> str:
     """index page"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
